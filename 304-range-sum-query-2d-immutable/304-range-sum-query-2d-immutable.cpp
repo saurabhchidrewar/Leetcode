474d4a54be1c71b29458f354782bbcc58d296e4d
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-02 00:10:14 +0530

    Time: 965 ms (8.84%), Space: 142.6 MB (23.02%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,31 +1,25 @@
-class NumMatrix
-{
-    vector<vector < int>> arr;
-    public:
-        NumMatrix(vector<vector < int>> &matrix)
-        {
-            for (int i = 0; i < matrix.size(); i++)
-            {
-                for (int j = 1; j < matrix[0].size(); j++)
-                {
-                    matrix[i][j] += matrix[i][j - 1];
-                }
-            }
-            arr = matrix;
-        }
 
-    int sumRegion(int row1, int col1, int row2, int col2)
-    {
-        int ans = 0;
-        for (int i = row1; i <= row2; i++)
-        {
-            ans += arr[i][col2];
-            if (col1 >= 1)
-                ans -= arr[i][col1 - 1];
+class NumMatrix {
+    vector<vector<int>> matSum;
+    int r,c,sum;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        r = matrix.size();
+        c = matrix[0].size();
+        matSum = vector<vector<int>>(r+1,vector<int>(c+1));
+        //Fill sum values
+        for(int i=1;i<=r;++i){
+            for(int j=1;j<=c;++j)
+                matSum[i][j] = matSum[i-1][j] + matSum[i][j-1] - matSum[i-1][j-1] + matrix[i-1][j-1];
         }
-        return ans;
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        sum = matSum[row2+1][col2+1] - matSum[row1][col2+1] - matSum[row2+1][col1] + matSum[row1][col1];
+        return sum;
     }
 };
+
 
 /**
  *Your NumMatrix object will be instantiated and called as such:
