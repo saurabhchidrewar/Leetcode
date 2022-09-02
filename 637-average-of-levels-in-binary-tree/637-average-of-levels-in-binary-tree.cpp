11390d98b1748fa5aa7030ee76807c99bf9dba4e
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-02 18:09:35 +0530

    Time: 34 ms (19.26%), Space: 22.6 MB (32.47%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -1,37 +1,42 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-    vector <double> ans;
-    void bfs(TreeNode* root) {
+class Solution
+{
+    vector<double> ans;
+    void bfs(TreeNode *root)
+    {
         if (root == NULL) return;
-        queue <TreeNode*> q;
+        queue<TreeNode*> q;
         q.push(root);
-        while (!q.empty()) {
+        while (!q.empty())
+        {
             double sm = 0, n = q.size();
-            for (int i = 0; i < (int)n; i++) {
-                TreeNode* temp = q.front();
-                if (temp->left) 
+            for (int i = 0; i < (int) n; i++)
+            {
+                TreeNode *temp = q.front();
+                if (temp->left)
                     q.push(temp->left);
                 if (temp->right)
                     q.push(temp->right);
                 sm += temp->val;
                 q.pop();
             }
-            ans.push_back(sm/n);
+            ans.push_back(sm / n);
         }
     }
-public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        bfs(root);
-        return ans;
-    }
+    public:
+        vector<double> averageOfLevels(TreeNode *root)
+        {
+            bfs(root);
+            return ans;
+        }
 };
