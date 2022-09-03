c1bd6b7b52e3a949e610aa5b4f3c732506f71ce6
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-03 16:39:45 +0530

    Time: 4 ms (58.93%), Space: 7.3 MB (82.84%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,23 +1,18 @@
-class Solution:
-    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        self.ans=set()
-        def recur(s):
-            if k==0 and s[-1]=='0':
-                return
-            if len(s)==n and s[0]!='0':
-                self.ans.add(int(s))
-                return
-            if len(s)==n+1 and s[0]=='0':
-                self.ans.add(int(s))
-                return
-            
-            x=int(s[-1])+k
-            y=int(s[-1])-k
-            
-            if 0<=x<10:
-                recur(s+str(x))
-            if 0<=y<10:
-                recur(s+str(y))
-        for i in range(10):
-            recur(str(i))
-        return list(self.ans)
+class Solution {
+public:
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector <int> arr = {1,2,3,4,5,6,7,8,9};
+        for (int i = 1; i < n; i++) {
+            vector <int> arr2;
+            for (auto c: arr) {
+                int x = c % 10;
+                if (x + k < 10)
+                    arr2.push_back(c * 10 + (x + k));
+                if (k > 0 && x - k >= 0)
+                    arr2.push_back(c * 10 + (x - k));
+            }
+            arr = arr2;
+        }
+        return arr;
+    }
+};
