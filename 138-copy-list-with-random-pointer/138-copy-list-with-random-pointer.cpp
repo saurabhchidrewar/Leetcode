3dde992107584f301f2029e427c7b106a14100bf
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-05 16:08:09 +0530

    Time: 21 ms (16.09%), Space: 11.4 MB (17.16%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -14,27 +14,39 @@ public:
 };
 */
 
-class Solution {
-public:
-    Node* copyRandomList(Node* head) {
-        map <Node*, Node*> mp;
-        Node* t = head;
-        Node* root = new Node(0);
-        Node* p = root;
-        while (t) {
-            Node* temp = new Node(t->val);
-            p->next = temp;
-            p = temp;
-            mp[t] = p;
-            t = t->next;
+class Solution
+{
+    public:
+        Node* copyRandomList(Node *head)
+        {
+           	// Iterate 2 times 
+           	// First iteration : Create the linked list without random 
+            //                   pointers and map new node  with the old node
+           	// Second iteration: In the newly created list connect random 
+            //                   pointers using the generated map
+
+            map<Node*, Node*> mp;
+            Node *t = head;
+            Node *root = new Node(0);
+            Node *p = root;
+            while (t)
+            {
+                Node *temp = new Node(t->val);
+                p->next = temp;
+                p = temp;
+                mp[t] = p;	
+                // Map new node with the old node such 
+                // that we can correlate random pointer in next pass
+                t = t->next;
+            }
+            p->next = NULL;
+            t = head, p = root->next;
+            while (t)
+            {
+                p->random = mp[t->random];
+                t = t->next;
+                p = p->next;
+            }
+            return root->next;
         }
-        p->next = NULL;
-        t = head, p = root->next;
-        while (t) {
-            p->random = mp[t->random];
-            t = t->next;
-            p = p->next;
-        }
-        return root->next;
-    }
 };
