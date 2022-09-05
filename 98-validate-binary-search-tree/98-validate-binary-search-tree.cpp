d2cd8ad39c2233cfdaafe0d41ef60380e01d9a0e
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-05 17:00:19 +0530

    Time: 27 ms (19.32%), Space: 24.7 MB (6.49%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,33 +1,40 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
 
-class Solution {
-    bool isBST(TreeNode* root, long mn, long mx) {
+class Solution
+{
+    map<TreeNode*, bool> mp;
+    bool isBST(TreeNode *root, long mn, long mx)
+    {
         if (root == NULL)
             return true;
-        
+
+        if (mp.find(root) != mp.end())
+            return mp[root];
+
         if ((root->val >= mx) || (root->val <= mn))
-            return false;
-        
+            return mp[root] = false;
+
         bool a = isBST(root->left, mn, root->val);
-        if (!a) return false;
-        
+        if (!a) return mp[root] = false;
+
         bool b = isBST(root->right, root->val, mx);
-        if (!b) return false;
-        
-        return true;
+        if (!b) return mp[root] = false;
+
+        return mp[root] = true;
     }
-public:
-    bool isValidBST(TreeNode* root) {
-        return isBST(root, LONG_MIN, LONG_MAX);
-    }
+    public:
+        bool isValidBST(TreeNode *root)
+        {
+            return isBST(root, LONG_MIN, LONG_MAX);
+        }
 };
