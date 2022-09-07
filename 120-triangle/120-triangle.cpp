a6a70655ca14657170da925c195964e879554fd6
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-07 16:59:14 +0530

    Time: 7 ms (81.68%), Space: 8.6 MB (78.85%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,20 +4,18 @@ public:
         int n = triangle.size();
         if (n == 1)
             return triangle[0][0];
-        vector <vector<int>> ans(n, vector<int>(n,0));
-        ans[0][0] = triangle[0][0];
         int mn = INT_MAX;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < triangle[i].size(); j++) {
                 if (j == 0)
-                    ans[i][j] = ans[i-1][j] + triangle[i][j];
+                    triangle[i][j] += triangle[i-1][j];
                 else if (j == triangle[i].size() - 1)
-                    ans[i][j] = ans[i-1][j-1] + triangle[i][j];
+                    triangle[i][j] += triangle[i-1][j-1];
                 else
-                    ans[i][j] = triangle[i][j] + min(ans[i-1][j], ans[i-1][j-1]);
+                    triangle[i][j] += min(triangle[i-1][j], triangle[i-1][j-1]);
                 
                 if (i == n-1) 
-                    mn = min(mn, ans[i][j]);
+                    mn = min(mn, triangle[i][j]);
             }
         }
         return mn;
