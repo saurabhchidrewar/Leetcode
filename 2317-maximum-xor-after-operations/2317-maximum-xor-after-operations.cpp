4ae102b0ab2000d95751ac28ebbf42b2a2be1121
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-11 13:28:23 +0530

    Time: 107 ms (84.18%), Space: 48 MB (48.90%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
         int ans = 0;
-        for (auto c: nums) ans |= c;
+        for (int i = 0; i < nums.size(); i++) ans = ans | nums[i];
         return ans;
     }
 };
