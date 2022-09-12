c8870e8092882a9e60f00e59d37291791cbb79a3
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-13 00:25:30 +0530

    Time: 4 ms (41.00%), Space: 6.1 MB (60.30%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     long long smallestNumber(long long num) {
-        string s = to_string(num);
+        string s = to_string(abs(num));
         sort(s.begin(), s.end());
-        if (s[0] != '-') {
-            int i = 0;
-            while (s.size() > 1 && i < s.size() && s[i] == '0') i++;
-            string temp = s;
-            s = temp[i];
-            for (int j = 0; j < temp.size(); j++) {
-                if (j == i) continue;
-                s += temp[j];
-            }
+        if (num <= 0) {
+            reverse(s.begin(), s.end());
+            return -1 * stoll(string(s.begin(), s.end()));
         } else {
-            reverse(s.begin()+1, s.end());
+            int i = 0;
+            while (s[i] == '0') i++;
+            swap(s[0], s[i]);
         }
-        long long ans = 0;
-        int power = 0;
-        for (int i = s.size()-1; i >= 0; i--) {
-            if (s[i] == '-') ans*=-1;
-            else {
-                ans += ((s[i] - '0') * pow(10, power));
-                power++;
-            }
-        }
-        return ans;
+        return stoll(s);
     }
 };
