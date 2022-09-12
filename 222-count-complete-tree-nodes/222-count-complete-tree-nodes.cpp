a14cb45e769b09357b2ea1ff91d21e72697e07fd
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-12 13:17:31 +0530

    Time: 70 ms (17.90%), Space: 30.9 MB (63.62%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,6 +9,10 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+// O(n) Solution
+
 class Solution {
 public:
     int countNodes(TreeNode* root) {
@@ -22,5 +26,41 @@ public:
             b = countNodes(root->right);
         
         return 1 + a + b;
+    }
+};
+*/
+
+// Optimal 
+// TC : O(log(n) * log(n))
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == NULL) return 0;
+        
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        
+        if (lh == rh) return (1 << lh) - 1;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+    
+    int leftHeight(TreeNode* root) {
+        int h = 0;
+        while (root) {
+            h++;
+            root = root->left;
+        }
+        return h;
+    }
+    
+    int rightHeight(TreeNode* root) {
+        int h = 0;
+        while (root) {
+            h++;
+            root = root->right;
+        }
+        return h;
     }
 };
