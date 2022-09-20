3a1d917c296219776b6a0f1f1ab2b65692d2245f
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-09-20 07:54:33 +0530

    Time: 73 ms (40.94%), Space: 19 MB (68.60%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,25 +1,33 @@
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        
-        vector <vector<int>> ans;
-        
-        for (auto &c : intervals) {
-            if (ans.size() == 0) {
-                ans.push_back({c[0],c[1]});
-                continue;
+class Solution
+{
+    public:
+        vector<vector < int>> merge(vector<vector < int>> &intervals)
+        {
+            sort(intervals.begin(), intervals.end());
+
+            vector<vector < int>> ans;
+
+            for (auto &c: intervals)
+            {
+                int a = c[0], b = c[1];
+                if (ans.size() == 0)
+                {
+                    ans.push_back({a, b});
+                    continue;
+                }
+
+                int m = ans[ans.size() - 1][0], n = ans[ans.size() - 1][1];
+
+                if (a >= m && a <= n)
+                {
+                    ans[ans.size() - 1][1] = max(b, n);
+                }
+                else
+                {
+                    ans.push_back({ a, b });
+                }
             }
-            
-            int a = c[0], b = c[1], m = ans[ans.size()-1][0], n = ans[ans.size()-1][1];
-            
-            if (a >= m && a <= n) {
-                ans[ans.size()-1][1] = max(b, n);
-            } else {
-                ans.push_back({a,b});
-            }
+
+            return ans;
         }
-        
-        return ans;
-    }
 };
