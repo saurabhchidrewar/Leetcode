9573addb5a23b464cbe05a237e6c784f9ae63789
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-01 08:42:36 +0530

    Time: 7 ms (23.68%), Space: 6.5 MB (27.31%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,3 +1,5 @@
+// Recursive
+/*
 class Solution {
     unordered_map <int,int> dp;
     int solve(string s, int i) {
@@ -15,5 +17,25 @@ public:
     int numDecodings(string s) {
         dp[s.size()] = 1;      
         return solve(s, 0);
+    }
+};
+*/
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        unordered_map <int,int> dp;
+        dp[s.size()] = 1;
+        for (int i = s.size()-1; ~i; i--) {
+            if (s[i] == '0')
+                dp[i] = 0;
+            else
+                dp[i] = dp[i+1];
+            
+            if (i+1 < s.size() && 
+                (s[i] == '1' || (s[i] == '2' && s[i+1] < '7')))
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
     }
 };
