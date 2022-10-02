5cda8f7386f18f7ea4b886feba068025d17d1779
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-02 11:22:23 +0530

    Time: 104 ms (19.73%), Space: 8.5 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,5 +1,4 @@
 // memoization
-
 /*
 class Solution {
     int mod = 1e9 + 7;
@@ -38,13 +37,13 @@ public:
         dp[0][0] = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= target; j++) {
-                long ans = 0;
+                int ans = 0;
                 for (int m = 1; m <= k; m++) {
                     if (j-m>=0)
-                        ans += dp[i-1][j-m] % mod;
+                        ans = (ans + dp[i-1][j-m]) % mod;
                     // ans += solve(n-1, k, target-i, dp) % mod;
                 }
-                dp[i][j] = ans % mod;
+                dp[i][j] = ans;
             }
         }
         return dp[n][target];
