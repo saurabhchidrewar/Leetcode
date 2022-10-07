50a67ccb23d7a58468286024b89b1dfdc8b8eca5
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-07 10:15:26 +0530

    Time: 8 ms (93.05%), Space: 10.6 MB (36.05%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,20 +1,19 @@
 class Solution {
     int m,n;
-    void traverse(vector<vector<int>>& rooms, vector<int>& vis, int i) {
-        if (vis[i]) return;
+    void traverse(vector<vector<int>>& rooms, vector<int>& vis, int &s, int i) {
         vis[i] = 1;
+        s -= i;
         for (auto c: rooms[i]) {
-            traverse(rooms, vis, c);
+            if (vis[c] == 0)
+                traverse(rooms, vis, s, c);
         }
     }
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         m = rooms.size();
         vector <int> vis(m, 0);
-        traverse(rooms, vis, 0);
-        for (int i = 0; i < m; i++) {
-            if (vis[i] == 0) return false;
-        }
-        return true;
+        int s = (m) * (m-1) / 2;
+        traverse(rooms, vis, s, 0);
+        return s == 0;
     }
 };
