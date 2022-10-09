460397e65daf25e1099a065c2299edf9848eba0d
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-09 18:07:46 +0530

    Time: 24 ms (18.85%), Space: 13.8 MB (90.55%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,22 @@
 class Solution {
     int m, n;
     typedef vector<vector<int>> vvi;
-    void dfs(vvi &image, vvi &vis, int i, int j, int color, int curr) {
+    void dfs(vvi &image, int i, int j, int color, int curr) {
         if (i < 0 || j < 0 || i >= m || j >= n || image[i][j] != curr
-            || vis[i][j]) 
+            || image[i][j] == color) 
             return;
         
         image[i][j] = color;
-        vis[i][j] = 1;
-        dfs(image, vis, i-1, j, color, curr);
-        dfs(image, vis, i+1, j, color, curr);
-        dfs(image, vis, i, j-1, color, curr);
-        dfs(image, vis, i, j+1, color, curr);
+        dfs(image, i-1, j, color, curr);
+        dfs(image, i+1, j, color, curr);
+        dfs(image, i, j-1, color, curr);
+        dfs(image, i, j+1, color, curr);
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         m = image.size();
         n = image[0].size();
-        
-        vector<vector<int>> vis(m, vector<int> (n, 0));
-        
-        dfs(image, vis, sr, sc, color, image[sr][sc]);
+        dfs(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 };
