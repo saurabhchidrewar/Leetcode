8881372d9100774fff7d11e8db86865712eaf056
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-09 10:46:16 +0530

    Time: 55 ms (33.17%), Space: 13.7 MB (89.71%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,26 +1,30 @@
-class Solution {
-    int n;
-    void dfs(vector<vector<int>>&grid, vector<int>& vis, int i) {
+class Solution
+{
+    void dfs(vector<vector < int>> &grid, vector< int > &vis, int i)
+    {
         vis[i] = 1;
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < vis.size(); j++)
+        {
             if (grid[i][j] == 1 && vis[j] == 0)
                 dfs(grid, vis, j);
         }
     }
-    
-public:
-    int findCircleNum(vector<vector<int>>& grid) {
-        n = grid.size();
-        int ans = 0;
-        vector <int> vis(n, 0);
-        
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                dfs(grid, vis, i);
-                ans++;
+
+    public:
+        int findCircleNum(vector<vector < int>> &grid)
+        {
+            int ans = 0;
+            vector<int> vis(grid.size(), 0);
+
+            for (int i = 0; i < vis.size(); i++)
+            {
+                if (vis[i] == 0)
+                {
+                    dfs(grid, vis, i);
+                    ans++;
+                }
             }
+
+            return ans;
         }
-        
-        return ans;
-    }
 };
