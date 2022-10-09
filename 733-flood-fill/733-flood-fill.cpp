196e884d0af23328a5b1eb4fc7678542197077bd
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-09 18:06:07 +0530

    Time: 19 ms (42.72%), Space: 14.3 MB (16.42%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,32 +1,26 @@
 class Solution {
     int m, n;
-    bool isValid(int i, int j, int m, int n) {
-        return !(i < 0 || j < 0 || i >= m || j >= n);
-    }
-    
-    void solve(vector<vector<int>>& image, int sr, int sc, int color, int val, vector <vector<int>> &vis) {
-        image[sr][sc] = color;
-        vis[sr][sc] = 1;
+    typedef vector<vector<int>> vvi;
+    void dfs(vvi &image, vvi &vis, int i, int j, int color, int curr) {
+        if (i < 0 || j < 0 || i >= m || j >= n || image[i][j] != curr
+            || vis[i][j]) 
+            return;
         
-        if (isValid(sr-1, sc, m, n) && !vis[sr-1][sc] && image[sr-1][sc] == val) 
-            solve(image, sr-1, sc, color, val, vis);
-        
-        if (isValid(sr+1, sc, m, n) && !vis[sr+1][sc] && image[sr+1][sc] == val) 
-            solve(image, sr+1, sc, color, val, vis);
-        
-        if (isValid(sr, sc-1, m, n) && !vis[sr][sc-1] && image[sr][sc-1] == val) 
-            solve(image, sr, sc-1, color, val, vis);
-        
-        if (isValid(sr, sc+1, m, n) && !vis[sr][sc+1] && image[sr][sc+1] == val) 
-            solve(image, sr, sc+1, color, val, vis);
-        
+        image[i][j] = color;
+        vis[i][j] = 1;
+        dfs(image, vis, i-1, j, color, curr);
+        dfs(image, vis, i+1, j, color, curr);
+        dfs(image, vis, i, j-1, color, curr);
+        dfs(image, vis, i, j+1, color, curr);
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         m = image.size();
         n = image[0].size();
-        vector <vector<int>> vis(m, vector<int>(n, 0));
-        solve(image, sr, sc, color, image[sr][sc], vis);
+        
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+        
+        dfs(image, vis, sr, sc, color, image[sr][sc]);
         return image;
     }
 };
