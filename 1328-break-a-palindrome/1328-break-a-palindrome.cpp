c0b9bf0b3fa75ac305007508475304683b92c0b3
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-11 02:18:50 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.46%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+        string breakPalindrome(string palindrome)
+        {
+            int n = palindrome.size() / 2;
+            if (n == 0) return "";
+            for (int i = 0; i < n; i++)
+            {
+                if (palindrome[i] != 'a')
+                {
+                    palindrome[i] = 'a';
+                    return palindrome;
+                }
+            }
+            palindrome[palindrome.size()-1] = 'b';
+            return palindrome;
+        }
+};
