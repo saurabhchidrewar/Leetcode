96dcbf39326e88da2a7fd889f2cc8a196907c8da
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-11 09:37:45 +0530

    Time: 74 ms (92.81%), Space: 61.6 MB (67.34%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,22 +1,43 @@
+// O(n) Time and O(n) Space - Accepted
+
+/*
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         int n = nums.size(), mn = nums[0];
         int mx = nums[n-1];
-        vector <int> small(n, 0), large(n, 0);
-        
+        vector<int> small(n, 0), large(n, 0);
+
         for (int i = 0; i < nums.size(); i++) {
             small[i] = min(mn, nums[i]);
             mn = min(mn, nums[i]);
-            
+
             large[n-i-1] = max(mx, nums[n-i-1]);
             mx = max(mx, nums[n-i-1]);
         }
-        
+
         for (int i = 0; i < n; i++) {
             if (nums[i] > small[i] && nums[i] < large[i]) return true;
         }
-        
+
         return false;
     }
+};
+*/
+
+// O(n) Time and O(1) Space
+class Solution
+{
+    public:
+        bool increasingTriplet(vector<int> &nums)
+        {
+            int a = INT_MAX, b = INT_MAX;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (a >= nums[i]) a = nums[i];
+                else if (b >= nums[i]) b = nums[i];
+                else return true;
+            }
+            return false;
+        }
 };
