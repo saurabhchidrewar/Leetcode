f7c1823d1bcc9ad87e1d32fd34234e7dd61b39a5
Saurabh Chidrewar <70994155+saurabhchidrewar@users.noreply.github.com>
2022-10-13 11:24:51 +0530

    Time: 142 ms (13.00%), Space: 30.8 MB (98.35%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,3 +1,6 @@
+// Approach - 1 : Accepted
+
+/*
 class Solution {
     int n;
     bool traverse(vector<int> &arr, vector<int> &vis, int i) {
@@ -20,5 +23,27 @@ public:
         vector <int> vis(n, 0);
         
         return traverse(arr, vis, start);
+    }
+};
+*/
+
+// Better
+
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int i) {
+        int n = arr.size();
+        if (i < 0 || i >= n || arr[i] < 0)
+            return false;
+        
+        if (arr[i] == 0)
+            return true;
+        
+        arr[i] *= -1; // Instead of vis array
+        
+        bool a = canReach(arr, i + arr[i]);
+        if (a) return true;
+        bool b = canReach(arr, i - arr[i]);
+        return b;
     }
 };
